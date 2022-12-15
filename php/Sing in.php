006378be88eb886ff10ea2211c551f58bbff48a7
German Garzon H <garzonhernandezgerman@gmail.com>
2022-12-15 18:04:53 -0500

    Avance Completo
    Se agregaron todas las paginas con sus respectivos estilos además del manual de usuario

---
+++ php/Sing in.php
@@ -0,0 +1,23 @@
+<?php
+
+	class Conexion{
+		private $host ="localhost";
+		private $user ="root";
+		private $password ="";
+		private $db = "banco_jcg";
+		private $conect;
+
+		public function __construct(){
+			$conectionString = "mysql:localhos=".$this->host."dbname".$this->db."charset=uft8";
+			try {
+				$this->connect = new PDO($conectionString,$this->user,$this->password);
+				$this->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				echo "Conexión Exitosa";
+			} catch (Exception $e) {
+				$this->connect = 'Error de Conexión';
+				echo "ERROR: ". $e->getMessage();
+			}
+		}
+	}
+	$conect = new Conexion();
+?>
